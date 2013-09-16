508aa7db723bd32bdc5a85c4d595f4d5af2d3e4d
Christian Sørensen <christian.braunersorensen@prevas.dk>
2013-09-16 12:44:31 +0000

    appwd: wdt_gpio: Enable use without gpio dt support
    Allow integration of wdt_gpio on machines that lack device-tree gpio
    support.

--- include/uapi/linux/appwd.h
+++ include/uapi/linux/appwd.h
@@ -25,6 +25,12 @@ extern void appwd_init_post_hook(void);
 #define appwd_init_post_hook() do {} while (0)
 #endif /* CONFIG_APPWD */
 
+#ifdef CONFIG_APPWD_WDT_GPIO
+extern int __init appwd_wdt_gpio_init(unsigned, int);
+#else
+#define appwd_wdt_gpio_init(a, b) do {} while (0)
+#endif /* CONFIG_APPWD_WDT_GPIO */
+
 #endif /* __KERNEL__ */
 
 #endif  /* _LINUX_APPWD_H */
