7ff68b7bf6adc2d8cdd0e1d6691eacfd0f2cdb21
Klaus Henning Sorensen <klaus.sorensen@prevas.dk>
2015-12-10 15:11:14 +0100

    appwd/wdm: Add missing dereference of pointer
    Added missing dereference of pointer arg to sizeof in memcpy call
    
    Signed-off-by: Esben Haabendal <eha@deif.com>

--- drivers/watchdog/appwd/wdm.c
+++ drivers/watchdog/appwd/wdm.c
@@ -204,7 +204,7 @@ appwd_wdt_register(const char * name, const struct wdt_operations * ops,
 			continue;
 
 		wdt[i].name = name;
-		memcpy(&wdt[i].ops, ops, sizeof(ops));
+		memcpy(&wdt[i].ops, ops, sizeof(*ops));
 		wdt[i].heartbeat_delay = heartbeat_delay;
 		wdt[i].data = data;
 		init_delayed_kthread_work(&wdt[i].heartbeat, wdt_heartbeat);
